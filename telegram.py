6b36c9705800bb9ddcbf87d1ad56c263e49a730a
n1lby73 <n1lby73@gmail.com>
2023-10-13 12:35:21 +0100

    removed comment and formatting

--- telegram.py
+++ telegram.py
@@ -10,32 +10,6 @@ load_dotenv()
 
 chatids = os.getenv('chatID', '').split(',')
 
-# def sendToTelegram(chatid, message):
-
-#     try:
-
-#         apiToken = os.getenv('botToken')
-
-#         chatID = chatid
-
-#         payload = {'chat_id': chatID, 'text': message}
-        
-#         apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
-
-#         response = requests.post(apiURL, data=payload)
-
-#         if response.status_code == 200:
-
-#             print("Message sent successfully!")
-
-#         else:
-
-#             print(f"Failed to send message: {response.text}")
-            
-#     except Exception as e:
-
-#         print(e)
-
 if len(sys.argv) != 3:
 
     print("Usage: telegram.py <Webhook URL> <IIOT Server URL>")
--- termuxService.sh
+++ termuxService.sh
@@ -1,32 +1,44 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # Paths to virtual environment activation scripts
+
 webhook_venv="$HOME/termuxWebhook/termuxenv/bin/activate"
 IIOT_venv="$HOME/industrial-IOT/iitenv/bin/activate"
 
 # Start the Webhook server locally
+
 source $webhook_venv
 cd $HOME/termuxWebhook
+
 export FLASK_APP=run.py
 export FLASK_DEBUG=True
+
 flask run --port 4962
 
 # Expose the server with Serveo for Webhook
+
 portForwarding=$(ssh -R 80:localhost:4962 serveo.net)
 webHookurl=$(echo "$portForwarding" | grep -o 'from [^ ]*' | awk '{print $2}')
+
 echo "Webhook: $webHookurl"
 
 # Start the IIOT server locally
+
 source $IIOT_venv
 cd $HOME/industrial-IOT/web-ui
+
 export FLASK_APP=run.py
 export FLASK_DEBUG=True
+
 flask run --port 5000
 
 # Expose the server with Serveo for IIOT
+
 portForwarding=$(ssh -R 4962:localhost:5000 serveo.net)
 IIOTurl=$(echo "$portForwarding" | grep -o 'from [^ ]*' | awk '{print $2}')
+
 echo "IIOT server: $IIOTurl"
 
 # Pass the URLs to a Python script
+
 telegram.py "$webHookurl" "$IIOTurl"
