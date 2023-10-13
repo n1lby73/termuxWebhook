d5405f4ca58ac90a3c474ee3c7460903f79428bb
n1lby73 <n1lby73@gmail.com>
2023-10-13 19:58:26 +0100

    Implemented failsafe

--- termuxService.sh
+++ termuxService.sh
@@ -1,5 +1,48 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+# Function to activate a virtual environment or exit on failure
+
+activate_venv() {
+
+    local venv_path="$1"
+    source "$venv_path" || { echo "Failed to activate virtual environment: $venv_path"; exit 1; }
+
+}
+
+# Function to run a Flask application in the background
+
+run_flask_app_background() {
+    
+    local port="$1"
+    nohup flask run --port "$port" &
+
+    if [ $? -ne 0 ]; then
+
+        echo "Failed to start Flask application on port $port"
+        exit 1
+
+    fi
+}
+
+# Function to expose a server with Serveo
+
+expose_with_serveo() {
+
+    local local_port="$1"
+    local serveo_port="$2"
+
+    local serveo_url=$(ssh -R "$serveo_port:localhost:$local_port" serveo.net)
+
+    if [ $? -ne 0 ]; then
+
+        echo "Failed to expose server on port $local_port with Serveo"
+        exit 1
+
+    fi
+
+    echo "$serveo_url"
+}
+
 # Paths to virtual environment activation scripts
 
 webhook_venv="$HOME/termuxWebhook/termuxenv/bin/activate"
@@ -7,40 +50,38 @@ IIOT_venv="$HOME/industrial-IOT/web-ui/iitenv/bin/activate"
 
 # Start the Webhook server locally
 
-source $webhook_venv
-cd $HOME/termuxWebhook
+activate_venv "$webhook_venv"
 
+cd "$HOME/termuxWebhook"
 export FLASK_APP=run.py
 export FLASK_DEBUG=True
 
-flask run --port 4962
+run_flask_app_background 4962
 
-# Expose the server with Serveo for Webhook
+# Expose the Webhook server with Serveo
 
-portForwarding=$(ssh -R 80:localhost:4962 serveo.net)
-webHookurl=$(echo "$portForwarding" | grep -o 'from [^ ]*' | awk '{print $2}')
+webHookurl=$(expose_with_serveo 4962 80)
 
 echo "Webhook: $webHookurl"
 
 # Start the IIOT server locally
 
-source $IIOT_venv
-cd $HOME/industrial-IOT/web-ui
+activate_venv "$IIOT_venv"
 
+cd "$HOME/industrial-IOT/web-ui"
 export FLASK_APP=run.py
 export FLASK_DEBUG=True
 
-flask run --port 5000
+run_flask_app_background 5000
 
-# Expose the server with Serveo for IIOT
+# Expose the IIOT server with Serveo
 
-portForwarding=$(ssh -R 4962:localhost:5000 serveo.net)
-IIOTurl=$(echo "$portForwarding" | grep -o 'from [^ ]*' | awk '{print $2}')
+IIOTurl=$(expose_with_serveo 5000 4962)
 
 echo "IIOT server: $IIOTurl"
 
 # Pass the URLs to a Python script
 
-source $webhook_venv
+source "$webhook_venv"
 
-pyhton ~/termuxWebhook/telegram.py "$webHookurl" "$IIOTurl"
+python ~/termuxWebhook/telegram.py "$webHookurl" "$IIOTurl"
