2cf65639ada02ade98cfcbfe284459fe167e33c1
n1lby73 <n1lby73@gmail.com>
2023-10-13 12:31:55 +0100

    imported send to telegram and verify signature function

--- server/serverApi.py
+++ server/serverApi.py
@@ -1,12 +1,10 @@
-from werkzeug.exceptions import Forbidden
+from server.customModules import sendToTelegram, verifyRequestSignature
 from flask_restful import Resource
 from dotenv import load_dotenv
 from server import api, app
 from flask import request
 import subprocess
 import requests
-import hashlib
-import hmac
 import os
 
 
@@ -14,46 +12,6 @@ load_dotenv()
 
 chatids = os.getenv('chatID', '').split(',')
 webhook_secret = os.getenv('GITHUB_WEBHOOK_SECRET')
-
-def verify_signature(payload_body, secret_token, signature_header):
-
-    if not signature_header:
-
-        raise Forbidden("x-hub-signature-256 header is missing")
-
-    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body.encode('utf-8'), digestmod=hashlib.sha256)
-
-    expected_signature = "sha256=" + hash_object.hexdigest()
-
-    if not hmac.compare_digest(expected_signature, signature_header):
-
-        raise Forbidden("Request signatures didn't match")
-
-def sendToTelegram(chatid, message):
-
-    try:
-
-        apiToken = os.getenv('botToken')
-
-        chatID = chatid
-
-        payload = {'chat_id': chatID, 'text': message}
-        
-        apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
-
-        response = requests.post(apiURL, data=payload)
-
-        if response.status_code == 200:
-
-            print("Message sent successfully!")
-
-        else:
-
-            print(f"Failed to send message: {response.text}")
-            
-    except Exception as e:
-
-        print(e)
 
 class pull(Resource):
 
@@ -68,7 +26,7 @@ class pull(Resource):
             payload_body = request.data.decode('utf-8')
 
             # Verify the GitHub Webhook payload
-            verify_signature(payload_body, webhook_secret, signature_header)
+            verifyRequestSignature(payload_body, webhook_secret, signature_header)
             
             command = "cd ~/industrial-IOT && git pull"
             result = subprocess.run(command, shell=True, capture_output=True, text=True)
